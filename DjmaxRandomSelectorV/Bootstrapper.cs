c69b4e112c694cb36164591a702e23f65d3c0b78
wowvv0w <wowvv0w@naver.com>
2022-01-24 22:42:08 +0900

    초기 커밋.

---
+++ DjmaxRandomSelectorV/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using DjmaxRandomSelectorV.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DjmaxRandomSelectorV
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
