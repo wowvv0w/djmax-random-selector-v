c90e31df7d3f93e32a29dfd682287293afe526f1
wowvv0w <wowvv0w@naver.com>
2022-02-13 20:39:47 +0900

    AllTrackList compatibility with 1.0.0 and 1.0.1

--- DjmaxRandomSelectorV/Models/Manager.cs
+++ DjmaxRandomSelectorV/Models/Manager.cs
@@ -18,7 +18,7 @@ namespace DjmaxRandomSelectorV.Models
         private const string PRESET_DEFAULT = "Default";
 
         private const string VERSION_URL = "https://raw.githubusercontent.com/wowvv0w/djmax-random-selector-v/main/DjmaxRandomSelectorV/Version.txt";
-        private const string ALL_TRACK_URL = "https://raw.githubusercontent.com/wowvv0w/djmax-random-selector-v/main/DjmaxRandomSelectorV/DataFiles/AllTrackList.csv";
+        private const string ALL_TRACK_URL = "https://raw.githubusercontent.com/wowvv0w/djmax-random-selector-v/main/DjmaxRandomSelectorV/AllTrackList.csv";
 
 
         public static Setting LoadSetting()
