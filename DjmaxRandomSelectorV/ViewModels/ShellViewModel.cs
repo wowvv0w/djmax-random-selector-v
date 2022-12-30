be5a2d77f75ff496d93811b93ff71c56dac801a2
wowvv0w <wowvv0w@naver.com>
2022-12-30 19:02:58 +0900

    Add top bar

--- DjmaxRandomSelectorV/ViewModels/ShellViewModel.cs
+++ DjmaxRandomSelectorV/ViewModels/ShellViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace DjmaxRandomSelectorV.ViewModels
 {
@@ -16,6 +17,30 @@ namespace DjmaxRandomSelectorV.ViewModels
         {
             var viewmodel = IoC.Get<MainViewModel>();
             await ActivateItemAsync(viewmodel, new CancellationToken());
+        }
+
+        public void MoveWindow(object view)
+        {
+            var window = view as Window;
+            window.DragMove();
+        }
+
+        public void MinimizeWindow(object view)
+        {
+            var window = view as Window;
+            window.WindowState = WindowState.Minimized;
+        }
+
+        public void CloseWindow(object view)
+        {
+            var window = view as Window;
+            window.Close();
+        }
+
+        public void OpenReleasePage()
+        {
+            string url = "https://github.com/wowvv0w/djmax-random-selector-v/releases";
+            System.Diagnostics.Process.Start(url);
         }
     }
 }
