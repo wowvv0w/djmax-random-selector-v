7ac1be0a702c89537cffb850f2e4510b22068651
wowvv0w <wowvv0w@naver.com>
2023-01-08 13:49:01 +0900

    Add filter handler
    Filter handler returns music list using track list and filter.

---
+++ Dmrsv.RandomSelector/IFilterHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Dmrsv.RandomSelector
+{
+    public interface IFilterHandler<TFilter> where TFilter : IFilter
+    {
+        IEnumerable<Music> Filter(IEnumerable<Track> trackList, TFilter filter);
+    }
+}
---
+++ Dmrsv.RandomSelector/IFilterHandlerBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dmrsv.RandomSelector
+{
+    public interface IFilterHandlerBuilder
+    {
+        void Reset();
+        void Build(FilterOption option);
+    }
+}
