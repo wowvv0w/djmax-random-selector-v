d7f297012ca18244c91957fe34f3d3b6cf4f2d4d
wowvv0w <wowvv0w@naver.com>
2023-01-18 21:44:01 +0900

    Add messages
    For event aggregator.

---
+++ DjmaxRandomSelectorV/Messages/SettingMessage.cs
@@ -0,0 +1,13 @@
+﻿using Dmrsv.RandomSelector;
+using System.Collections.Generic;
+
+namespace DjmaxRandomSelectorV.Messages
+{
+    public class SettingMessage
+    {
+        public FilterType FilterType { get; set; }
+        public int InputInterval { get; set; }
+        public bool SavesExclusion { get; set; }
+        public List<string> OwnedDlcs { get; set; }
+    }
+}
