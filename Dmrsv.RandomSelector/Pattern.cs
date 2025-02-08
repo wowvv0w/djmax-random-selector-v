986591f79f2f1f404328749d12209ce4f51f451a
wowvv0w <wowvv0w@naver.com>
2025-02-08 23:26:53 +0900

    Add new track datatype for 3.0

---
+++ Dmrsv.RandomSelector/Pattern.cs
@@ -0,0 +1,54 @@
+﻿namespace Dmrsv.RandomSelector
+{
+    public record Pattern
+    {
+        public int Id { get; init; } = -1;
+        public int Level { get; init; } = 0;
+        public int SongId => Id / 100;
+        public string ButtonTunes => (Id / 10 % 10) switch
+        {
+            4 => "4B",
+            5 => "5B",
+            6 => "6B",
+            8 => "8B",
+            _ => throw new NotSupportedException("Invalid ButtonTunes")
+        };
+        public string Difficulty => (Id % 10) switch
+        {
+            1 => "NM",
+            2 => "HD",
+            3 => "MX",
+            4 => "SC",
+            _ => throw new NotSupportedException("Invalid Difficulty")
+        };
+        public string Style => $"{ButtonTunes}{Difficulty}";
+
+        public Pattern(int id, int level)
+        {
+            Id = id;
+            Level = level;
+        }
+
+        public Pattern(int songId, string style, int level)
+        {
+            int buttonTunes = style[..2] switch
+            {
+                "4B" => 4,
+                "5B" => 5,
+                "6B" => 6,
+                "8B" => 8,
+                _ => throw new NotSupportedException("Invalid ButtonTunes")
+            };
+            int difficulty = style[2..4] switch
+            {
+                "NM" => 1,
+                "HD" => 2,
+                "MX" => 3,
+                "SC" => 4,
+                _ => throw new NotSupportedException("Invalid Difficulty")
+            };
+            Id = songId * 100 + buttonTunes * 10 + difficulty;
+            Level = level;
+        }
+    }
+}
--- Dmrsv.RandomSelector/Track.cs
+++ Dmrsv.RandomSelector/Track.cs
@@ -2,16 +2,17 @@
 {
     public record Track
     {
+        public int Id { get; init; } = -1;
         public string Title { get; init; } = string.Empty;
+        public string Composer { get; init; } = string.Empty;
         public string Category { get; init; } = string.Empty;
-        public Dictionary<string, int> Patterns { get; init; } = new();
+        public Dictionary<string, int> PatternLevelTable { get; init; } = new();
 
-        public IEnumerable<Music> GetMusicList()
+        public IEnumerable<Pattern> GetPatterns()
         {
-            var musicList = from p in Patterns
-                            select new Music(Title, p.Key, p.Value);
-
-            return musicList;
+            var patterns = from p in PatternLevelTable
+                           select new Pattern(Id, p.Key, p.Value);
+            return patterns;
         }
     }
 }
