fee5b8c5f5c078df37a8bdfb1e7920870531f3cd
wowvv0w <wowvv0w@naver.com>
2025-02-10 17:46:31 +0900

    Add class for deserializing appdata & Add some properties to appdata

---
+++ DjmaxRandomSelectorV/Dmrsv3CoreAppData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DjmaxRandomSelectorV.Models;
+
+namespace DjmaxRandomSelectorV
+{
+    public class Dmrsv3CoreAppData
+    {
+        public string DataFolderName { get; set; }
+        public string OldDataFolderName { get; set; }
+        public string ConfigFileName { get; set; }
+        public string AllTrackFileName { get; set; }
+        public string AllTrackDownloadUrl { get; set; }
+        public string AllTrackDownloadUrlAlt { get; set; }
+        public string CurrentFilterFileName { get; set; }
+        public string VersionCheckUrl { get; set; }
+        public string HomepageUrl { get; set; }
+        public string[] CategoryType { get; set; }
+        public string[] BasicCategories { get; set; }
+        public Category[] Categories { get; set; }
+        public (int Id, string[][] RequiredDlc)[] LinkDisc { get; set; }
+        public string GameWindowTitle { get; set; }
+    }
+}
