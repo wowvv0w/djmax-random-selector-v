99c05347d79f1dab21ce4de36fee608fedda84c1
wowvv0w <wowvv0w@naver.com>
2025-04-16 00:19:03 +0900

    Fix performance issue

--- DjmaxRandomSelectorV/ViewModels/FavoriteViewModel.cs
+++ DjmaxRandomSelectorV/ViewModels/FavoriteViewModel.cs
@@ -10,11 +10,20 @@ namespace DjmaxRandomSelectorV.ViewModels
 {
     public class FavoriteViewModel : Screen
     {
+        private const int ItemCountPerPage = 20;
         private readonly IEventAggregator _eventAggregator;
         private readonly TrackDB _db;
 
         private string _searchBox;
+        private string _pageText;
+        private string _itemCountText;
+        private bool _filtersAllTrack;
+        private bool _filtersFavorite;
+        private bool _filtersBlacklist;
         private List<FavoriteItem> _items;
+        private List<FavoriteItem> _searchedItems;
+        private int _pageCount; // 0-indexed
+        private int _currentPage;
 
         public string SearchBox
         {
@@ -23,14 +32,61 @@ namespace DjmaxRandomSelectorV.ViewModels
             {
                 _searchBox = value;
                 NotifyOfPropertyChange();
-                UpdateResult();
+                GetSearchedItems();
+            }
+        }
+        public string PageText
+        {
+            get { return _pageText; }
+            set
+            {
+                _pageText = value;
+                NotifyOfPropertyChange();
+            }
+        }
+        public string ItemCountText
+        {
+            get { return _itemCountText; }
+            set
+            {
+                _itemCountText = value;
+                NotifyOfPropertyChange();
+            }
+        }
+        public bool FiltersAllTrack
+        {
+            get { return _filtersAllTrack; }
+            set
+            {
+                _filtersAllTrack = value;
+                NotifyOfPropertyChange();
+                GetSearchedItems();
+            }
+        }
+        public bool FiltersFavorite
+        {
+            get { return _filtersFavorite; }
+            set
+            {
+                _filtersFavorite = value;
+                NotifyOfPropertyChange();
+                GetSearchedItems();
+            }
+        }
+        public bool FiltersBlacklist
+        {
+            get { return _filtersBlacklist; }
+            set
+            {
+                _filtersBlacklist = value;
+                NotifyOfPropertyChange();
+                GetSearchedItems();
             }
         }
 
         public BindableCollection<FavoriteItem> SearchResult { get; }
         public BindableCollection<FavoriteItem> FavoriteItems { get; }
         public BindableCollection<FavoriteItem> BlacklistItems { get; }
-        public BindableCollection<string> TitleSuggestions { get; }
 
         public FavoriteViewModel(IEventAggregator eventAggregator)
         {
@@ -45,8 +101,14 @@ namespace DjmaxRandomSelectorV.ViewModels
                 IsPlayable = track.IsPlayable, // TODO: apply the track is playable (required to respond to change setting)
                 Status = favorite.Contains(track.Id) ? 1 : (blacklist.Contains(track.Id) ? -1 : 0)
             }).OrderBy(item => item.Title).ToList();
-            SearchResult = new BindableCollection<FavoriteItem>(_items);
-            TitleSuggestions = new BindableCollection<string>();
+            _searchedItems = new List<FavoriteItem>();
+            _filtersAllTrack = true;
+            _filtersFavorite = false;
+            _filtersBlacklist = false;
+            _pageCount = 0;
+            _currentPage = 0;
+            SearchResult = new BindableCollection<FavoriteItem>();
+            GetSearchedItems();
         }
 
         public void CloseDialog()
@@ -64,23 +126,53 @@ namespace DjmaxRandomSelectorV.ViewModels
             TryCloseAsync(true);
         }
 
+        public void GetSearchedItems()
+        {
+            var searched = _items.Select(item => item);
+            if (FiltersFavorite)
+            {
+                searched = searched.Where(item => item.Status == 1);
+            }
+            else if (FiltersBlacklist)
+            {
+                searched = searched.Where(item => item.Status == -1);
+            }
+            if (!string.IsNullOrEmpty(SearchBox))
+            {
+                searched = SearchBox.Equals("#")
+                           ? searched.Where(item => !Regex.IsMatch(item.Title[..1], "[a-z]", RegexOptions.IgnoreCase))
+                           : searched.Where(item => item.Title.StartsWith(SearchBox, StringComparison.OrdinalIgnoreCase));
+            }
+            _searchedItems = searched.ToList();
+            _pageCount = _searchedItems.Count / ItemCountPerPage;
+            _currentPage = 0;
+            UpdateResult();
+        }
+
         public void UpdateResult()
         {
             SearchResult.Clear();
-            if (string.IsNullOrEmpty(SearchBox))
-            {
-                SearchResult.AddRange(_items);
-            }
-            var tracks = SearchBox.Equals("#")
-                         ? _items.Where(item => !Regex.IsMatch(item.Title[..1], "[a-z]", RegexOptions.IgnoreCase))
-                         : _items.Where(item => item.Info.Title.StartsWith(SearchBox, StringComparison.OrdinalIgnoreCase));
-            if (tracks is null)
+            int start = ItemCountPerPage * _currentPage;
+            int count = Math.Min(ItemCountPerPage, _searchedItems.Count - start);
+            SearchResult.AddRange(_searchedItems.GetRange(start, count));
+            PageText = $"{_currentPage + 1} / {_pageCount + 1}";
+            ItemCountText = $"{start+1}-{start+count} / {_searchedItems.Count}";
+        }
+
+        public void TurnPage(int toward)
+        {
+            if (toward == 1 && _currentPage == _pageCount)
             {
                 return;
             }
-            SearchResult.AddRange(tracks);
+            if (toward == -1 && _currentPage == 0)
+            {
+                return;
+            }
+            _currentPage += toward;
+            UpdateResult();
         }
-
+        /*
         public void SearchTitle()
         {
             TitleSuggestions.Clear();
@@ -114,7 +206,7 @@ namespace DjmaxRandomSelectorV.ViewModels
             }
             SearchBox = title;
         }
-
+        */
         public void ClearSearchBox()
         {
             SearchBox = string.Empty;
