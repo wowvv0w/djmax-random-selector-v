24f4af3b04378d7d1de1efba4a9aa983f69949c5
wowvv0w <wowvv0w@naver.com>
2025-04-16 22:36:15 +0900

    Add update manager

--- DjmaxRandomSelectorV/UpdateManager.cs
+++ DjmaxRandomSelectorV/UpdateManager.cs
@@ -1,43 +1,106 @@
 ﻿using System;
-using System.Net.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace DjmaxRandomSelectorV
 {
     public class UpdateManager
     {
-        private const string VersionCheckUrl = "https://raw.githubusercontent.com/wowvv0w/djmax-random-selector-v/refs/heads/feature-new-db/DjmaxRandomSelectorV/Version3.txt";
-        
-        public Version CurrentAppVersion { get; private set; }
-        public Version LatestAppVersion { get; private set; }
-        public int AllTrackVersion { get; private set; }
+        // TODO: restore url to main
+        private const string VersionCheckUrl = "https://raw.githubusercontent.com/wowvv0w/djmax-random-selector-v/refs/heads/develop-ver3/DjmaxRandomSelectorV/Version3.txt";
+        private const string AllTrackDownloadUrl = "https://v-archive.net/db/songs.json";
+        private const string AppdataDownloadUrl = "https://raw.githubusercontent.com/wowvv0w/djmax-random-selector-v/refs/heads/develop-ver3/DjmaxRandomSelectorV/DMRSV3_Data/appdata.json";
+        private const string AllTrackFilePath = @"DMRSV3_Data\AllTrackList.json";
+        private const string AppdataFilePath = @"DMRSV3_Data\appdata.json";
 
-        public UpdateManager(Dmrsv3Configuration config, Version appVer)
+        private readonly VersionContainer _container;
+        private readonly IFileManager _fileManager;
+
+        public UpdateManager(Dmrsv3Configuration config, VersionContainer container, IFileManager fileManager)
         {
-            CurrentAppVersion = appVer;
-            LatestAppVersion = appVer;
-            AllTrackVersion = config.VersionInfo.AllTrackVersion;
+            _container = container;
+            _fileManager = fileManager;
+            Version assemblyVersion = Assembly.GetEntryAssembly().GetName().Version;
+            _container.CurrentAppVersion = _container.LatestAppVersion = assemblyVersion;
+            var versionInfo = config.VersionInfo;
+            _container.AllTrackVersion = versionInfo.AllTrackVersion;
+            _container.AppdataVersion = versionInfo.AppdataVersion;
         }
 
-        public bool[] CheckUpdates()
+        public async Task UpdateAsync()
         {
-            using var client = new HttpClient();
-
-            string result;
+            string[] versions; // [ app version, appdata version, notice header, notice body ]
             try
             {
-                result = client.GetStringAsync(VersionCheckUrl).Result;
+                string result = await _fileManager.RequestAsync(VersionCheckUrl);
+                versions = result.Split('\n');
             }
             catch
             {
-                throw new Exception("Failed to check lastest versions.");
+                throw new Exception("Failed to check update.");
+            }
+            _container.LatestAppVersion = new Version(versions[0]);
+
+            var tasks = new List<Task<int>>();
+            // update all track
+            DateTime now = DateTime.Now, past = DateTime.MinValue;
+            if (!File.Exists(AppdataFilePath)
+                || !DateTime.TryParse(_container.AllTrackVersion, out past)
+                || now.CompareTo(past) > 0)
+            {
+                tasks.Add(DownloadAllTrackAsync());
+            }
+            // update appdata
+            if (versions[1].CompareTo(_container.AppdataVersion) > 0)
+            {
+                tasks.Add(DownloadAppdataAsync());
             }
 
-            string[] versions = result.Split('\n');
-            LatestAppVersion = new Version(versions[0]);
-            int latestAllTrackVersion = int.Parse(versions[1]);
-            bool allTrackUpdateAvailable = AllTrackVersion != latestAllTrackVersion;
-            AllTrackVersion = latestAllTrackVersion;
-            return new bool[] { CurrentAppVersion < LatestAppVersion, allTrackUpdateAvailable };
+            while (tasks.Count > 0)
+            {
+                var finishedTask = await Task.WhenAny(tasks);
+                int result = await finishedTask;
+                switch (result)
+                {
+                    case 0:
+                        _container.AllTrackVersion = now.ToString("g");
+                        break;
+                    case 1:
+                        _container.AppdataVersion = versions[1];
+                        break;
+                }
+                tasks.Remove(finishedTask);
+            }
+        }
+
+        private async Task<int> DownloadAllTrackAsync()
+        {
+            try
+            {
+                string result = await _fileManager.RequestAsync(AllTrackDownloadUrl);
+                _fileManager.Write(result, AllTrackFilePath);
+            }
+            catch
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        private async Task<int> DownloadAppdataAsync()
+        {
+            try
+            {
+                string result = await _fileManager.RequestAsync(AppdataDownloadUrl);
+                _fileManager.Write(result, AppdataFilePath);
+            }
+            catch
+            {
+                return -1;
+            }
+            return 1;
         }
     }
 }
---
+++ DjmaxRandomSelectorV/VersionContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DjmaxRandomSelectorV
+{
+    public class VersionContainer
+    {
+        public Version CurrentAppVersion { get; set; }
+        public Version LatestAppVersion { get; set; }
+        public string AllTrackVersion { get; set; }
+        public string AppdataVersion { get; set; }
+    }
+}
