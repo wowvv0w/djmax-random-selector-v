d3a79553d6a4c7157a3cdf76e912a58bbc3dbd5e
wowvv0w <wowvv0w@naver.com>
2025-07-27 21:22:27 +0900

    Add new filter and selector

---
+++ Dmrsv.RandomSelector/Filter.cs
@@ -0,0 +1,26 @@
+﻿namespace Dmrsv.RandomSelector
+{
+    public static class Filter
+    {
+        public static IEnumerable<Track> Sift(IEnumerable<Track> tracks, Func<Track, bool> trackCondition)
+        {
+            return tracks.Where(t => trackCondition(t));
+        }
+
+        public static IEnumerable<Pattern> Sift(IEnumerable<Pattern> patterns, Func<Pattern, bool> patternCondition)
+        {
+            return patterns.Where(p => patternCondition(p));
+        }
+
+        public static IEnumerable<Pattern> Sift(IEnumerable<Track> tracks, Func<Track, bool> trackCondition, Func<Pattern, bool> patternCondition)
+        {
+            var patterns = Sift(tracks, trackCondition).SelectMany(t => t.Patterns);
+            return Sift(patterns, patternCondition);
+        }
+
+        public static IEnumerable<Pattern> GroupwiseExtract<TGroupKey>(IEnumerable<Pattern> patterns, Func<Pattern, TGroupKey> groupKeySelector, Func<IEnumerable<Pattern>, Pattern> resultSelector)
+        {
+            return patterns.GroupBy(groupKeySelector).Select(g => resultSelector(g));
+        }
+    }
+}
---
+++ Dmrsv.RandomSelector/Selector.cs
@@ -0,0 +1,19 @@
+﻿namespace Dmrsv.RandomSelector
+{
+    public static class Selector
+    {
+        public static Pattern? SelectFrom(IReadOnlyList<Pattern> candidates, int index)
+        {
+            if (candidates is null)
+            {
+                return null;
+            }
+            if (index < 0 || index >= candidates.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return candidates[index];
+        }
+    }
+}
