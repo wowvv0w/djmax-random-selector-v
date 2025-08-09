4b8940855977e3ff29149d805488edd044a22a95
wowvv0w <wowvv0w@naver.com>
2025-08-10 00:07:46 +0900

    Add new history

---
+++ DjmaxRandomSelectorV/Services/History.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace DjmaxRandomSelectorV.Services
+{
+    public class History : Queue<int>, IHistory, IEnumerable<int>, IReadOnlyCollection<int>
+    {
+        private int _limit = 5;
+        public int Limit
+        {
+            get { return _limit; }
+            set
+            {
+                _limit = value;
+                while (Count > _limit)
+                {
+                    Dequeue();
+                }
+            }
+        }
+
+        public History(IEnumerable<int> recents) : base(recents) { }
+
+        public new void Enqueue(int item)
+        {
+            while (Count >= _limit)
+            {
+                Dequeue();
+            }
+            base.Enqueue(item);
+        }
+    }
+}
---
+++ DjmaxRandomSelectorV/Services/IHistory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DjmaxRandomSelectorV.Services
+{
+    public interface IHistory : IEnumerable<int>, IReadOnlyCollection<int>
+    {
+        int Limit { get; set; }
+        void Enqueue(int item);
+        int Dequeue();
+        void Clear();
+    }
+}
