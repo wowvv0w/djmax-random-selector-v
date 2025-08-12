c14eebe941589e2cfdd2da496e60bd1f33a11b4b
wowvv0w <wowvv0w@naver.com>
2025-08-12 17:47:43 +0900

    Delete unnecessary classes

--- DjmaxRandomSelectorV/States/IStateChangedNotifier.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DjmaxRandomSelectorV.States
-{
-    public interface IStateChangedNotifier<TState>
-    {
-        event StateChangedEventHandler<TState> OnStateChanged;
-        TState GetState();
-        void SetState(TState state);
-    }
-}
