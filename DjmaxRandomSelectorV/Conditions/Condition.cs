32153e260927eefe9445703fcc0cfff370811e9d
wowvv0w <wowvv0w@naver.com>
2025-08-14 01:02:25 +0900

    Add helper class for conditions

---
+++ DjmaxRandomSelectorV/Conditions/Condition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace DjmaxRandomSelectorV.Conditions
+{
+    public static class Condition
+    {
+        public static NullCondition Null => NullCondition.Instance;
+
+        public static ICondition ComplementOf(ICondition condition)
+        {
+            if (condition is ComplementCondition complement)
+            {
+                return complement.Condition;
+            }
+            return new ComplementCondition(condition);
+        }
+
+        public static UnionCondition CreateUnion(params (bool IsEnabled, Func<ICondition> Generate)[] queries)
+        {
+            var result = queries.Where(query => query.IsEnabled).Select(query => query.Generate()).Where(cond => cond is not null);
+            return result.Any() ? new UnionCondition(result) : null;
+        }
+
+        public static IntersectionCondition CreateIntersection(params (bool IsEnabled, Func<ICondition> Generate)[] queries)
+        {
+            var result = queries.Where(query => query.IsEnabled).Select(query => query.Generate()).Where(cond => cond is not null);
+            return result.Any() ? new IntersectionCondition(result) : null;
+        }
+    }
+}
---
+++ DjmaxRandomSelectorV/Conditions/NullCondition.cs
@@ -0,0 +1,16 @@
+﻿using Dmrsv.RandomSelector;
+
+namespace DjmaxRandomSelectorV.Conditions
+{
+    public class NullCondition : ICondition
+    {
+        private static NullCondition _instance = null;
+        private NullCondition() { }
+        public static NullCondition Instance => _instance ??= new NullCondition();
+
+        public bool IsSatisfiedBy(Pattern pattern)
+        {
+            return false;
+        }
+    }
+}
