b34c12a39afd5725af6147e14f9c4893143af7e8
wowvv0w <wowvv0w@naver.com>
2025-08-15 00:18:43 +0900

    Add empty locator for test

---
+++ DjmaxRandomSelectorV/Services/LocatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dmrsv.RandomSelector;
+
+namespace DjmaxRandomSelectorV.Services
+{
+    public class LocatorService : ILocator
+    {
+        public void Locate(Pattern pattern)
+        {
+            // TODO: implementation
+        }
+    }
+}
