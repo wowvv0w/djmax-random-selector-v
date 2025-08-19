5faeafba37e5715624694967c23d5c377b40a939
wowvv0w <wowvv0w@naver.com>
2025-08-19 18:40:26 +0900

    Prepare migration

--- DjmaxRandomSelectorV/App.xaml.cs
+++ DjmaxRandomSelectorV/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using DjmaxRandomSelectorV.ViewModels;
+using DjmaxRandomSelectorV.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DjmaxRandomSelectorV
 {
@@ -13,5 +17,20 @@ namespace DjmaxRandomSelectorV
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                .AddTransient<MainWindowViewModel>()
+                .BuildServiceProvider());
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
---
+++ DjmaxRandomSelectorV/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DjmaxRandomSelectorV.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _name = "Hello, World!";
+    }
+}
