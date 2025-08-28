f58b5a8b91c8e6c4ee0205ff3aa5ddb9a8b3980b
wowvv0w <wowvv0w@naver.com>
2025-08-28 23:32:47 +0900

    Fix error XDG0008, XDG0066

--- DjmaxRandomSelectorV.Ui/Controls/SettingToggleButton.cs
+++ DjmaxRandomSelectorV.Ui/Controls/SettingToggleButton.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace DjmaxRandomSelectorV.Ui.Controls
 {
     public class SettingToggleButton : ToggleButton
     {
+        static SettingToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SettingToggleButton), new FrameworkPropertyMetadata(typeof(SettingToggleButton)));
+        }
     }
 }
