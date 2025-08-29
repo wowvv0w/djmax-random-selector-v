1699291d71240e08b6fb2fbe0a00935f26d55a52
wowvv0w <wowvv0w@naver.com>
2025-08-29 15:28:18 +0900

    Add setting spinbox

---
+++ DjmaxRandomSelectorV.Ui/Controls/SettingSpinBox.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DjmaxRandomSelectorV.Ui.Controls
+{
+    public class SettingSpinBox : SettingControl
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value",
+            typeof(int),
+            typeof(SettingSpinBox),
+            new FrameworkPropertyMetadata(
+                0,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public static readonly DependencyProperty MininumProperty = DependencyProperty.Register(
+            "Minimum",
+            typeof(int),
+            typeof(SettingSpinBox),
+            new FrameworkPropertyMetadata(0));
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register(
+            "Maximum",
+            typeof(int),
+            typeof(SettingSpinBox),
+            new FrameworkPropertyMetadata(10));
+        public static readonly DependencyProperty ValueTextProperty = DependencyProperty.Register(
+            "ValueText",
+            typeof(string),
+            typeof(SettingSpinBox),
+            new FrameworkPropertyMetadata(null));
+        public static readonly DependencyProperty IsCircularProperty = DependencyProperty.Register(
+            "IsCircular",
+            typeof(bool),
+            typeof(SettingSpinBox),
+            new PropertyMetadata(false));
+        public static readonly DependencyProperty DelayProperty = DependencyProperty.Register(
+            "Delay",
+            typeof(int),
+            typeof(SettingSpinBox),
+            new PropertyMetadata(250));
+        public static readonly DependencyProperty IntervalProperty = DependencyProperty.Register(
+            "Interval",
+            typeof(int),
+            typeof(SettingSpinBox),
+            new PropertyMetadata(33));
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        public int Minimum
+        {
+            get => (int)GetValue(MininumProperty);
+            set => SetValue(MininumProperty, value);
+        }
+        public int Maximum
+        {
+            get => (int)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+        public string ValueText
+        {
+            get => (string)GetValue(ValueTextProperty);
+            set => SetValue(ValueTextProperty, value);
+        }
+        public bool IsCircular
+        {
+            get => (bool)GetValue(IsCircularProperty);
+            set => SetValue(IsCircularProperty, value);
+        }
+        public int Delay
+        {
+            get => (int)GetValue(DelayProperty);
+            set => SetValue(DelayProperty, value);
+        }
+        public int Interval
+        {
+            get => (int)GetValue(IntervalProperty);
+            set => SetValue(IntervalProperty, value);
+        }
+
+        private RepeatButton? _prevValueButtonElement;
+        public RepeatButton? PrevValueButtonElement
+        {
+            get => _prevValueButtonElement;
+            set
+            {
+                if (_prevValueButtonElement is not null)
+                {
+                    _prevValueButtonElement.Click -= new RoutedEventHandler(PrevValueButtonElement_Click);
+                }
+                _prevValueButtonElement = value;
+                if (_prevValueButtonElement is not null)
+                {
+                    _prevValueButtonElement.Click += new RoutedEventHandler(PrevValueButtonElement_Click);
+                }
+            }
+        }
+
+        private RepeatButton? _nextValueButtonElement;
+        public RepeatButton? NextValueButtonElement
+        {
+            get => _nextValueButtonElement;
+            set
+            {
+                if (_nextValueButtonElement is not null)
+                {
+                    _nextValueButtonElement.Click -= new RoutedEventHandler(NextValueButtonElement_Click);
+                }
+                _nextValueButtonElement = value;
+                if (_nextValueButtonElement is not null)
+                {
+                    _nextValueButtonElement.Click += new RoutedEventHandler(NextValueButtonElement_Click);
+                }
+            }
+        }
+
+        static SettingSpinBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SettingSpinBox), new FrameworkPropertyMetadata(typeof(SettingSpinBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            PrevValueButtonElement = GetTemplateChild("PrevValueButton") as RepeatButton;
+            NextValueButtonElement = GetTemplateChild("NextValueButton") as RepeatButton;
+        }
+
+        private void SetValue(int newValue)
+        {
+            if (Minimum <= newValue && newValue <= Maximum)
+            {
+                Value = newValue;
+            }
+            else if (IsCircular)
+            {
+                int count = Maximum - Minimum + 1;
+                Value = (newValue + count) % count;
+            }
+        }
+
+        private void PrevValueButtonElement_Click(object sender, RoutedEventArgs e)
+        {
+            SetValue(Value - 1);
+        }
+        private void NextValueButtonElement_Click(object sender, RoutedEventArgs e)
+        {
+            SetValue(Value + 1);
+        }
+    }
+}
--- DjmaxRandomSelectorV/ViewModels/FilterOptionViewModel.cs
+++ DjmaxRandomSelectorV/ViewModels/FilterOptionViewModel.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using DjmaxRandomSelectorV.Enums;
 using DjmaxRandomSelectorV.Messages;
-using DjmaxRandomSelectorV.Models;
 using DjmaxRandomSelectorV.Services;
 using DjmaxRandomSelectorV.States;
 
@@ -43,25 +43,42 @@ namespace DjmaxRandomSelectorV.ViewModels
                 NotifyOfPropertyChange();
             }
         }
+        public int ModeValue
+        {
+            get => (int)_filterOption.Mode;
+            set
+            {
+                _filterOption.Mode = (MusicForm)value;
+                NotifyOfPropertyChange();
+                NotifyOfPropertyChange(nameof(ModeText));
+            }
+        }
+        public int AiderValue
+        {
+            get => (int)_filterOption.Aider;
+            set
+            {
+                _filterOption.Aider = (InputMethod)value;
+                NotifyOfPropertyChange();
+                NotifyOfPropertyChange(nameof(AiderText));
+            }
+        }
+        public int LevelValue
+        {
+            get => (int)_filterOption.Level;
+            set
+            {
+                _filterOption.Level = (LevelPreference)value;
+                NotifyOfPropertyChange();
+                NotifyOfPropertyChange(nameof(LevelText));
+            }
+        }
         public string ModeText { get => _modeItems[_filterOption.Mode]; }
         public string AiderText { get => _aiderItems[_filterOption.Aider]; }
         public string LevelText { get => _levelItems[_filterOption.Level]; }
-
-        public object ExceptCountUpdater { get; }
-        public object ModeUpdater { get; }
-        public object AiderUpdater { get; }
-        public object LevelUpdater { get; }
-
-        private bool _testBoolean = true;
-        public bool TestBoolean
-        {
-            get => _testBoolean;
-            set
-            {
-                _testBoolean = value;
-                NotifyOfPropertyChange();
-            }
-        }
+        public int ModeMaxValue { get => Enum.GetValues<MusicForm>().Length - 1; }
+        public int AiderMaxValue { get => Enum.GetValues<InputMethod>().Length - 1; }
+        public int LevelMaxValue { get => Enum.GetValues<LevelPreference>().Length - 1; }
 
         public FilterOptionViewModel(IEventAggregator eventAggregator, IFilterOptionStateManager filterOptionManager)
         {
@@ -69,41 +86,6 @@ namespace DjmaxRandomSelectorV.ViewModels
             _filterOptionManager = filterOptionManager;
             _filterOption = _filterOptionManager.GetFilterOption();
             _filterOptionManager.OnFilterOptionStateChanged += PublishMessage;
-
-            ExceptCountUpdater = new SettingSliderItem(
-                "EXCLUDE RECENT MUSICS",
-                0,
-                30,
-                1,
-                () => _filterOption.RecentsCount,
-                newValue => _filterOption.RecentsCount = newValue);
-
-            ModeUpdater = new SettingSpinBoxItem(
-                "MODE",
-                () => (int)_filterOption.Mode,
-                newValue => _filterOption.Mode = (MusicForm)newValue,
-                (int)MusicForm.Default,
-                (int)MusicForm.Free,
-                true,
-                value => _modeItems[(MusicForm)value]);
-
-            AiderUpdater = new SettingSpinBoxItem(
-                "AIDER",
-                () => (int)_filterOption.Aider,
-                newValue => _filterOption.Aider = (InputMethod)newValue,
-                (int)InputMethod.Default,
-                (int)InputMethod.NotInput,
-                true,
-                value => _aiderItems[(InputMethod)value]);
-
-            LevelUpdater = new SettingSpinBoxItem(
-                "LEVEL",
-                () => (int)_filterOption.Level,
-                newValue => _filterOption.Level = (LevelPreference)newValue,
-                (int)LevelPreference.None,
-                (int)LevelPreference.Highest,
-                true,
-                value => _levelItems[(LevelPreference)value]);
 
             ActivateItemAsync(IoC.Get<FilterOptionIndicatorViewModel>());
         }
@@ -120,32 +102,6 @@ namespace DjmaxRandomSelectorV.ViewModels
         private void PublishMessage(IFilterOptionState filterOption)
         {
             _eventAggregator.PublishOnUIThreadAsync(new FilterOptionMessage(filterOption));
-        }
-
-        public void SwitchMode()
-        {
-            int value = (int)_filterOption.Mode;
-            value ^= 0x1;
-            _filterOption.Mode = (MusicForm)value;
-            NotifyOfPropertyChange(nameof(ModeText));
-        }
-
-        public void SwitchAider(int move)
-        {
-            int value = (int)_filterOption.Aider;
-            value += move;
-            value = (value % 3 + 3) % 3;
-            _filterOption.Aider = (InputMethod)value;
-            NotifyOfPropertyChange(nameof(AiderText));
-        }
-
-        public void SwitchLevel(int move)
-        {
-            int value = (int)_filterOption.Level;
-            value += move;
-            value = (value % 3 + 3) % 3;
-            _filterOption.Level = (LevelPreference)value;
-            NotifyOfPropertyChange(nameof(LevelText));
         }
     }
 }
